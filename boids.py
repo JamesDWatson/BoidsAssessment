69f4b36f92fd3c5f90c2a8998736e28f81c84a98
James Watson <05watson.j@gmail.com>
2017-02-19 17:59:16 +0000

    Removed some hard constant

--- boids.py
+++ boids.py
@@ -5,35 +5,41 @@ import random
 
 # Deliberately terrible code for teaching purposes
 
-boids_x=[random.uniform(-450,50.0) for x in range(50)]
+boids_x=[random.uniform(-450,50.0) for x in range(50)]               #Generates the x and y coordinates of boids.
 boids_y=[random.uniform(300.0,600.0) for x in range(50)]
-boid_x_velocities=[random.uniform(0,10.0) for x in range(50)]
+boid_x_velocities=[random.uniform(0,10.0) for x in range(50)]        #Generates the velocities of the boids.
 boid_y_velocities=[random.uniform(-20.0,20.0) for x in range(50)]
 boids=(boids_x,boids_y,boid_x_velocities,boid_y_velocities)
 
+posit_weight = 0.01
+vel_weight = 0.125
+position_difference = 10
+vel_difference = 100
+
 def update_boids(boids):
     xs,ys,xvs,yvs=boids
+    
     # Fly towards the middle
     for i in range(len(xs)):
         for j in range(len(xs)):
-            xvs[i]=xvs[i]+(xs[j]-xs[i])*0.01/len(xs)
+            xvs[i]=xvs[i]+(xs[j]-xs[i])*posit_weight/len(xs)
     for i in range(len(xs)):
         for j in range(len(xs)):
-            yvs[i]=yvs[i]+(ys[j]-ys[i])*0.01/len(xs)
+            yvs[i]=yvs[i]+(ys[j]-ys[i])*posit_weight/len(xs)
             
     # Fly away from nearby boids
     for i in range(len(xs)):
         for j in range(len(xs)):
-            if (xs[j]-xs[i])**2 + (ys[j]-ys[i])**2 < 100:
+            if (xs[j]-xs[i])**2 + (ys[j]-ys[i])**2 < position_difference**2:
                 xvs[i]=xvs[i]+(xs[i]-xs[j])
                 yvs[i]=yvs[i]+(ys[i]-ys[j])
                 
     # Try to match speed with nearby boids
     for i in range(len(xs)):
         for j in range(len(xs)):
-            if (xs[j]-xs[i])**2 + (ys[j]-ys[i])**2 < 10000:
-                xvs[i]=xvs[i]+(xvs[j]-xvs[i])*0.125/len(xs)
-                yvs[i]=yvs[i]+(yvs[j]-yvs[i])*0.125/len(xs)
+            if (xs[j]-xs[i])**2 + (ys[j]-ys[i])**2 < vel_difference**2:
+                xvs[i]=xvs[i]+(xvs[j]-xvs[i])*vel_weight/len(xs)
+                yvs[i]=yvs[i]+(yvs[j]-yvs[i])*vel_weight/len(xs)
                 
     # Move according to velocities
     for i in range(len(xs)):
