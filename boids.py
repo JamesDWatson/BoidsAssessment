31509c896c158a5482c9758b891c16eeee7fa010
James Watson <05watson.j@gmail.com>
2017-02-19 18:36:06 +0000

    Merged more For loops

--- boids.py
+++ boids.py
@@ -4,6 +4,7 @@ from matplotlib import animation
 import random
 
 # Deliberately terrible code for teaching purposes
+# Replace the Pythagoras theorem with function.
 
 boids_x=[random.uniform(-450,50.0) for x in range(50)]               #Generates the x and y coordinates of boids.
 boids_y=[random.uniform(300.0,600.0) for x in range(50)]
@@ -24,21 +25,15 @@ def update_boids(boids):
         for j in range(len(x_pos)):
             x_vel[i]=x_vel[i]+(x_pos[j]-x_pos[i])*posit_weight/len(x_pos)
             y_vel[i]=y_vel[i]+(y_pos[j]-y_pos[i])*posit_weight/len(x_pos)
- 
             
-    # Fly away from nearby boids
-    for i in range(len(x_pos)):
-        for j in range(len(x_pos)):
             if (x_pos[j]-x_pos[i])**2 + (y_pos[j]-y_pos[i])**2 < position_difference**2:
                 x_vel[i]=x_vel[i]+(x_pos[i]-x_pos[j])
                 y_vel[i]=y_vel[i]+(y_pos[i]-y_pos[j])
-                
-    # Try to match speed with nearby boids
-    for i in range(len(x_pos)):
-        for j in range(len(x_pos)):
+            
             if (x_pos[j]-x_pos[i])**2 + (y_pos[j]-y_pos[i])**2 < vel_difference**2:
                 x_vel[i]=x_vel[i]+(x_vel[j]-x_vel[i])*vel_weight/len(x_pos)
                 y_vel[i]=y_vel[i]+(y_vel[j]-y_vel[i])*vel_weight/len(x_pos)
+                
                 
     # Move according to velocities
     for i in range(len(x_pos)):
